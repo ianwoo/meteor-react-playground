dc03c37e38cc97f444040f16d8f0bb40c76b2674
ianwoo <Ian@Ians-MacBook-Pro-3.local>
2018-11-28 17:33:55 +0800

    added clock and toggle components

--- client/main.js
+++ client/main.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../imports/ui/app.js';
 import Calc from '../imports/ui/calc.js';
+import Clock from '../imports/ui/clock.js';
+import Toggle from '../imports/ui/toggle.js'
 
 Meteor.startup(() => {
   ReactDOM.render(<App dumbass={'kanye'} fucker={'trump'}/>, document.getElementById('app'));
   ReactDOM.render(<App dumbass={'hillary'} fucker={'duterte'}/>, document.getElementById('app2'));
   ReactDOM.render(<Calc input1={2} input2={2}/>, document.getElementById('calc'));
+  ReactDOM.render(<Clock name={'world'}/>, document.getElementById('clock'));
+  ReactDOM.render(<Toggle/>, document.getElementById('toggle'));
 });
